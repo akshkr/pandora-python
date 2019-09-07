de55c7d431f2b9a957ecc58647cad98d012d6db8
Akash Kumar <akash@sumyag.com>
2019-09-07 21:56:45 +0530

    Code Refactored and add Null percentage

--- sketch/__init__.py
+++ sketch/__init__.py
@@ -1,2 +1,9 @@
+# Main io function to make canvas object
 from sketch.io.parsers import outline
+
+# Models
+# Stacking
 from sketch.models.stacks import stack
+
+# Analyser
+from sketch.util.analyser import analyse
